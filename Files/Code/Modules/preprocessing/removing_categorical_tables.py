028a328da0d55f5ba37c8b80078a0ba82ae38e33
João Vítor Fernandes Dias <joaovitorfd2000@gmail.com>
2025-09-20 16:34:11 -0300

    Limitando os valores usados

--- Files/Code/Modules/preprocessing/removing_categorical_tables.py
+++ Files/Code/Modules/preprocessing/removing_categorical_tables.py
@@ -5,6 +5,23 @@ def get_numerical_table(tables) -> pd.DataFrame:
     transactions = tables["transacoes"]
     # Drop columns
     cols_to_drop = [
+      'discount',
+      'distributor_id',
+      'gross_profit',
+      'gross_value',
+      # 'internal_product_id', # Mandatory
+      # 'internal_store_id', # Mandatory
+      'net_value',
+      # 'quantity', # Mandatory
+      'reference_date',
+      'taxes',
+      # 'transaction_date' # Mandatory
+    ]
+    # print(transactions.dtypes) # List columns and types
+    transactions = transactions.drop(columns=cols_to_drop)
+    # print(transactions.dtypes) # List columns and types
+
+    cols_to_convert = [
       # 'discount',
       # 'distributor_id',
       # 'gross_profit',
@@ -12,26 +29,9 @@ def get_numerical_table(tables) -> pd.DataFrame:
       # 'internal_product_id',
       # 'internal_store_id',
       # 'net_value',
-      # 'quantity',
-      'reference_date',
-      # 'taxes',
-      'transaction_date'
-    ]
-    # print(transactions.dtypes) # List columns and types
-    transactions = transactions.drop(columns=cols_to_drop)
-    # print(transactions.dtypes) # List columns and types
-
-    cols_to_convert = [
-      'discount',
-      'distributor_id',
-      'gross_profit',
-      'gross_value',
-      'internal_product_id',
-      'internal_store_id',
-      'net_value',
       'quantity',
       # 'reference_date',
-      'taxes',
+      # 'taxes',
       # 'transaction_date'
     ]
     # Convert those columns types to int

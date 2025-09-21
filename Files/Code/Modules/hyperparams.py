f1a3cf04681845dc29e2311aff0d1a3622aac0e5
rangel-avila <rangelavilabarroso@gmail.com>
2025-09-21 17:43:39 -0300

    testing generalized

--- Files/Code/Modules/hyperparams.py
+++ Files/Code/Modules/hyperparams.py
@@ -16,9 +16,9 @@ def get_hyperparams():
         # Neural Network Global Parameters
         'input_size': 30,       # Number of past days to use as input
         'output_size': 7,       # Number of future days to predict
-        'batch_size': 28,       # Batch size for training
+        'batch_size': 80,       # Batch size for training
         'n_layers': 4,          # Number of layers in the N-BEATS model
-        'hidden_size': 128,     # Number of hidden units in each layer
+        'hidden_size': 64,     # Number of hidden units in each layer
 
         # Training parameters
         'learning_rate': 1e-3,  # Learning rate for the optimizer
@@ -27,6 +27,6 @@ def get_hyperparams():
         'blind_horizon': 4,     # Number of days to exclude from the end of the training set for hard test
         'split': 1,             # Proportion of data to use for training (1.0 for validation)
         'seed': 42,             # Random seed for reproducibility
-        'patience': 5,          # Early stopping patience
+        'patience': 9034,          # Early stopping patience
     }
     return HYPERPARAMS
--- Files/Code/Modules/models/training.py
+++ Files/Code/Modules/models/training.py
@@ -37,7 +37,7 @@ def train_model(model, learning_rate, epochs, device, dataloader, hyperparams):
         if hyperparams['debug']['verbose']:
             size = len(str(epochs))
             print(f"Epoch {epoch+1:0{size}d}/{epochs}, Loss: {total_loss/len(dataloader):.4f}")
-    print(f"Treinamento concluído. Loss final: {total_loss/len(dataloader):.4f}")
+    print(f"Treinamento concluído. Loss final: {total_loss/len(dataloader):.4f}", end='\t')
     return model
 
 
